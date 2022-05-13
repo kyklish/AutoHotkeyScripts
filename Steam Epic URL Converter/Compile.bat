b045b207c7b6e028907c0c6b80feb9fbc9e66891
kyklish <kyklish@gmail.com>
2022-05-13 20:09:53 +0300

    Steam Epic URL Converter.ahk - initial commit

---
+++ Steam Epic URL Converter/Compile.bat
@@ -0,0 +1,1 @@
+"D:\SERGEY\Options\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /base "D:\SERGEY\Options\Program Files\AutoHotkey\Compiler\Unicode 32-bit.bin" /compress 1 /in "Steam Epic URL Converter.ahk"
