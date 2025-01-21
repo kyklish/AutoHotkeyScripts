bd2c35f7465e2591274a70a3c841e8cdf8aac6d4
kyklish <kyklish@gmail.com>
2025-01-21 14:01:55 +0200

    Steam_Epic_URL_Converter\Compile.bat - use 64-bit ahk

--- Steam_Epic_URL_Converter/Compile.bat
+++ Steam_Epic_URL_Converter/Compile.bat
@@ -1,1 +1,1 @@
-"D:\SERGEY\Options\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /base "D:\SERGEY\Options\Program Files\AutoHotkey\Compiler\Unicode 32-bit.bin" /compress 1 /in "Steam_Epic_URL_Converter.ahk"
+"%SOFT_AHK%\Compiler\Ahk2Exe.exe" /base "%SOFT_AHK%\Compiler\Unicode 64-bit.bin" /compress 1 /in "Steam_Epic_URL_Converter.ahk"
